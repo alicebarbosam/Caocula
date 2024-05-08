afc992df1888c1a81de96f0e366764b78c9ab0e8
alicebarbosam <alicebmenezes@hotmail.com>
2024-05-08 18:00:11 -0300

    implementa coisas

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State var texto: String = ""
     @State var years: Int? = nil
     @State var months: Int? = nil
+    @State var result: Int?
     
     var body: some View {
         
@@ -23,6 +24,31 @@ struct ContentView: View {
                 
             Text("Meses")
             TextField("E quantos meses além disso ele tem.", value: $months, format: .number)
+            
+            Text("Porte")
+            //aqui fica segmented control
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+                
+            } else {
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: .infinity)
+                    .shadow(radius: 20)
+            }
+            Button(action: {result = 23}, label: {
+                ZStack{
+                    Color.purple
+                    Text("Cãocular")
+                        .foregroundStyle(.white)
+                }
+            })
+            .cornerRadius(10)
+            .frame(height: 50)
+                
                 
         }
         .padding()
